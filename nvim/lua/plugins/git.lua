cbe6c70d5d6945822c33dab10b42eef142ae293b
Akira Baruah <akira.baruah@gmail.com>
2024-04-20 00:50:53 -0700

    nvim: Add Fugitive with basic keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,4 +1,15 @@
 return {
+  { -- Git wrapper for Neovim. Fugitive reminds me of Magit from Emacs.
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gg', vim.cmd.Git, desc = 'Git (Fugitive)' },
+      { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
+
+      -- Custom keymaps for the Fugitive buffer.
+      { '<Tab>', '=', ft = 'fugitive', remap = true, desc = 'Toggle inline diff at cursor' },
+      { 'q', vim.cmd.quit, ft = { 'fugitive', 'fugitiveblame' }, desc = 'Quit Fugitive' },
+    },
+  },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
