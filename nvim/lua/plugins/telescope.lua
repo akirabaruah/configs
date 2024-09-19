9d1d88f54478ed3499b0ea3210584030f9c99901
Akira Baruah <akira.baruah@gmail.com>
2024-09-19 22:41:01 +0000

    nvim: Use telescope-live-grep-args.nvim
    Lets me use ripgrep args in my search queries.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,6 +13,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
         return vim.fn.executable 'make' == 1
       end,
     },
+    {
+      'nvim-telescope/telescope-live-grep-args.nvim',
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = '^1.0.0',
+    },
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
@@ -20,6 +26,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
   },
   config = function()
     local actions = require 'telescope.actions'
+    local lga_actions = require 'telescope-live-grep-args.actions'
     require('telescope').setup {
       defaults = {
         mappings = {
@@ -35,6 +42,15 @@ return { -- Fuzzy Finder (files, lsp, etc)
         },
       },
       extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ['<C-g>'] = lga_actions.quote_prompt { postfix = ' --iglob ' },
+            },
+          },
+        },
+
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
         },
@@ -43,6 +59,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- Enable Telescope extensions if they are installed.
     pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'live_grep_args')
     pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
@@ -53,11 +70,13 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+    local live_grep_args = require('telescope').extensions.live_grep_args.live_grep_args
+    vim.keymap.set('n', '<leader>sg', live_grep_args, { desc = '[S]earch by [G]rep' })
 
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
